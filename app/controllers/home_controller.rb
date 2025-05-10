887459ad6d8c3eacc0ff17bc33d7d2d126265f0b
Your Name <your.email@example.com>
2025-05-10 01:36:53 -0400

    adding repos to landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     # Set flash alert message if auth_message parameter is present
     flash.now[:alert] = params[:auth_message] if params[:auth_message].present?
+
+    @featured_repositories = Repository.order(created_at: :desc).limit(6)
   end
 end
